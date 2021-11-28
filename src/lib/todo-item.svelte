ce3318a35bf71977dfe09a0bb2662f7cd8f2d57a
Mike Nikles <mikenikles@pm.me>
2021-11-28 21:05:19 +0000

    Enhance HTML forms.

--- src/lib/todo-item.svelte
+++ src/lib/todo-item.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+  import { enhance } from "$lib/actions/form";
+
   export let todo: Todo;
+  export let processDeletedTodoResult: (res: Response) => void;
+  export let processUpdatedTodoResult: (res: Response) => void;
 </script>
 
 <style>
@@ -82,17 +86,23 @@
 </style>
 
 <div class="todo" class:done={todo.done}>
-  <form action="/todos/{todo.uid}.json?_method=patch" method="post">
+  <form action="/todos/{todo.uid}.json?_method=patch" method="post" use:enhance={{
+    result: processUpdatedTodoResult
+  }}>
     <input type="hidden" name="done" value="{todo.done ? '' : 'true'}" />
     <button aria-label="Mark todo as {todo.done ? 'not done' : 'done'}" class="toggle"></button>
   </form>
 
-  <form action="/todos/{todo.uid}.json?_method=patch" method="post" class="text">
+  <form action="/todos/{todo.uid}.json?_method=patch" method="post" class="text" use:enhance={{
+    result: processUpdatedTodoResult
+  }}>
     <input type="text" name="text" value="{todo.text}" />
     <button aria-label="Save todo" class="save"></button>
   </form>
   
-  <form action="/todos/{todo.uid}.json?_method=delete" method="post">
+  <form action="/todos/{todo.uid}.json?_method=delete" method="post" use:enhance={{
+    result: processDeletedTodoResult
+  }}>
     <button aria-label="Delete todo" class="delete"></button>
   </form>
 </div>
